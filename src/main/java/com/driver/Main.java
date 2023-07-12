cdfc99fc0f12db3ef4a3441b759d55b345a9a984
Wajid <ansariwajid9999@gmail.com>
2023-07-12 18:21:20 +0530

    code changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
+import com.sun.nio.sctp.SctpSocketOption;
 
-public class Main {
+public class Main
+{
+    public static class Product{
+        public Product(){}
+        public int product(int x,int y)
+        {
+            return x*y;
+        }
+        public  int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
 
+    public static void main(String[]args)
+    {
+        Product p=new Product();
+        p.product(10,20);
+        p.product(10,20,30);
+        p.product(2.2,1.020);
+    }
 }
